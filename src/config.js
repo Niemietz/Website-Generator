1a2ea45bdcab8763a03362fdc542392f1a109a1a
Niemietz <renan_ncs@msn.com>
2026-09-03 20:21:39 +0100

    Deploy beta version

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_SERVER_URL = 'localhost:3000';
-const USE_HTTPS = false;
+const API_SERVER_URL = 'website-generator-8f410bf0e46b.herokuapp.com';
+const USE_HTTPS = true;
 
 export const API_BASE_URL = `${USE_HTTPS ? 'https' : 'http'}://${API_SERVER_URL}`;
